30882614f81f33624573ad0f214e7d2416938b34
Ricardo Lu <shenglu1202@163.com>
2023-04-14 23:23:32 +0800

    feat: implement RTCP handler part to receive and send RTCP packets.
    1.Support parse RTCP RR.
    2.Send original RTCP report blocks received from RR without process, thus the report blocks' data in SR is meaningless.

--- RtpRtcpHandler.cpp
+++ RtpRtcpHandler.cpp
@@ -6,11 +6,13 @@
 #include "Logger.h"
 #include <random>
 #include <cstring>
+#include <sys/time.h>
 
 RtpRtcpHandler::RtpRtcpHandler(const int server_rtp_port, const int server_rtcp_port,
                                const std::string& ip, int rtp_port, int rtcp_port, uint32_t timestamp_increment)
        : ip_(ip), rtp_port_(rtp_port), rtcp_port_(rtcp_port), rtp_sequence_number_(0),
-       timestamp_(0), timestamp_increment_(timestamp_increment) {
+       timestamp_(0), timestamp_increment_(timestamp_increment), is_running_(true),
+       total_payload_size_(0), total_rtp_packet_sent_(0) {
     // Generate a random SSRC
     std::random_device rd;
     std::mt19937 mt(rd());
@@ -19,7 +21,7 @@ RtpRtcpHandler::RtpRtcpHandler(const int server_rtp_port, const int server_rtcp_
 
     rtp_socket_ = socket(AF_INET, SOCK_DGRAM, 0);
     if (rtp_socket_ < 0) {
-        throw std::runtime_error("RtpRtcpHandler create server socket failed");
+        throw std::runtime_error("RtpRtcpHandler create server rtp socket failed");
     }
 
     int opt = 1;
@@ -27,18 +29,52 @@ RtpRtcpHandler::RtpRtcpHandler(const int server_rtp_port, const int server_rtcp_
         throw std::runtime_error("Error setting rtp_socket_ options");
     }
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(server_rtp_port);
-    addr.sin_addr.s_addr = inet_addr("0.0.0.0");
+    struct sockaddr_in rtp_addr;
+    rtp_addr.sin_family = AF_INET;
+    rtp_addr.sin_port = htons(server_rtp_port);
+    rtp_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
 
-    if (bind(rtp_socket_, (struct sockaddr*)&addr, sizeof(struct sockaddr)) < 0) {
+    if (bind(rtp_socket_, (struct sockaddr*)&rtp_addr, sizeof(struct sockaddr)) < 0) {
         throw std::runtime_error("Bind rtp socket with server failed.");
     }
+
+    rtcp_socket_ = socket(AF_INET, SOCK_DGRAM, 0);
+    if (rtcp_socket_ < 0) {
+        throw std::runtime_error("RtpRtcpHandler create server rtcp socket failed");
+    }
+
+    opt = 1;
+    if (setsockopt(rtcp_socket_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        throw std::runtime_error("Error setting rtp_socket_ options");
+    }
+
+    struct sockaddr_in rtcp_addr;
+    rtcp_addr.sin_family = AF_INET;
+    rtcp_addr.sin_port = htons(server_rtcp_port);
+    rtcp_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+    if (bind(rtcp_socket_, (struct sockaddr*)&rtcp_addr, sizeof(struct sockaddr)) < 0) {
+        throw std::runtime_error("Bind rtp socket with server failed.");
+    }
+
+    rtcp_send_thread_.reset(new std::thread(&RtpRtcpHandler::send_rtcp_packet, this));
+    rtcp_receive_thread_.reset(new std::thread(&RtpRtcpHandler::receive_rtcp_packet, this));
 }
 
 RtpRtcpHandler::~RtpRtcpHandler() {
-    close(rtp_socket_);
+    is_running_ = false;
+    if (rtcp_send_thread_ && rtcp_send_thread_->joinable()) {
+        rtcp_send_thread_->join();
+        rtcp_send_thread_.reset();
+    }
+
+    if (rtcp_receive_thread_ && rtcp_receive_thread_->joinable()) {
+        rtcp_receive_thread_->join();
+        rtcp_receive_thread_.reset();
+    }
+
+    if (rtp_socket_ >= 0) close(rtp_socket_);
+    if (rtcp_socket_ >= 0) close(rtcp_socket_);
 }
 
 void RtpRtcpHandler::send_h264(const char* payload, size_t payload_size) {
@@ -91,7 +127,7 @@ void RtpRtcpHandler::send_h264(const char* payload, size_t payload_size) {
             client_addr_.sin_family = AF_INET;
             client_addr_.sin_port = htons(rtp_port_);
             if (inet_pton(AF_INET, ip_.c_str(), &client_addr_.sin_addr) <= 0) {
-                throw std::runtime_error("RtpRtcpHandler create client udp socket failed");
+                throw std::runtime_error("RtpRtcpHandler bind client rtp socket address failed");
             }
             size_t rtp_packet_size = header_size + 2 + fragment_size;
             ssize_t ret = sendto(rtp_socket_, rtp_buf_, rtp_packet_size, 0,
@@ -102,6 +138,7 @@ void RtpRtcpHandler::send_h264(const char* payload, size_t payload_size) {
             first_fragment = false;
         }
         timestamp_ += timestamp_increment_;
+        ++total_rtp_packet_sent_;
     } else {
         // Create the RTP packet without fragmentation
         RTPHeader rtp_header;
@@ -124,14 +161,15 @@ void RtpRtcpHandler::send_h264(const char* payload, size_t payload_size) {
         client_addr_.sin_family = AF_INET;
         client_addr_.sin_port = htons(rtp_port_);
         if (inet_pton(AF_INET, ip_.c_str(), &client_addr_.sin_addr) <= 0) {
-            throw std::runtime_error("RtpRtcpHandler create client udp socket failed");
+            throw std::runtime_error("RtpRtcpHandler bind client rtp socket address failed");
         }
         size_t rtp_packet_size = header_size + payload_size - start_code;
-        ssize_t  ret = sendto(rtp_socket_, rtp_buf_, rtp_packet_size, 0,
+        ssize_t ret = sendto(rtp_socket_, rtp_buf_, rtp_packet_size, 0,
                               (struct sockaddr *)&client_addr_, sizeof(client_addr_));
         if ((payload[start_code] & 0x1F) != 7 && (payload[start_code] & 0x1F) != 8) {   // Skip SPS & PPS
             timestamp_ += timestamp_increment_;
         }
+        ++total_rtp_packet_sent_;
     }
 }
 
@@ -160,12 +198,13 @@ void RtpRtcpHandler::send_aac(const char* payload, size_t payload_size) {
     client_addr_.sin_family = AF_INET;
     client_addr_.sin_port = htons(rtp_port_);
     if (inet_pton(AF_INET, ip_.c_str(), &client_addr_.sin_addr) <= 0) {
-        throw std::runtime_error("RtpRtcpHandler create client udp socket failed");
+        throw std::runtime_error("RtpRtcpHandler bind client rtp socket address failed");
     }
     size_t rtp_packet_size = header_size + 4 + payload_size;
     ssize_t  ret = sendto(rtp_socket_, rtp_buf_, rtp_packet_size, 0,
                           (struct sockaddr *)&client_addr_, sizeof(client_addr_));
     timestamp_ += timestamp_increment_;
+    ++total_rtp_packet_sent_;
 }
 
 void RtpRtcpHandler::send_rtp_packet(const char* payload, size_t payload_size, MediaType payload_type) {
@@ -180,12 +219,125 @@ void RtpRtcpHandler::send_rtp_packet(const char* payload, size_t payload_size, M
             throw std::runtime_error("Not support media type, broken.");
             break;
     }
+    total_payload_size_ += payload_size;
+}
+
+bool RtpRtcpHandler::parse_rtcp_packet(const char* buffer, int received_bytes, RTCPReceiverReport& rr) {
+    if (received_bytes < static_cast<int>(sizeof(RTCPHeader))) {
+        LOG_WARN("Not enough data for an RTCP packet");
+        return false;
+    }
+
+    RTCPHeader header;
+    memcpy(&header, buffer, sizeof(RTCPHeader));
+
+    if (header.version_ != 2 || header.packet_type_ != 201) {
+        LOG_WARN("Invalid RTCP version or not an RTCP RR packet");
+        return false;
+    }
+    rr.version_ = header.version_;
+    rr.packet_type_ = header.packet_type_;
+    uint16_t length = ntohs(header.length_);
+    if (received_bytes < static_cast<int>(sizeof(RTCPHeader) + length * 4)) {
+        LOG_WARN("Incomplete RTCP packet");
+        return false;
+    }
+    rr.padding_ = header.padding_;
+    rr.length_ = length;
+    memcpy(&rr.ssrc_, buffer + sizeof(RTCPHeader), 4);
+    rr.ssrc_ = ntohl(rr.ssrc_);
+
+    std::lock_guard<std::mutex> lock(rtcp_send_receive_mutex_);
+    for (int i = 0; i < header.report_count_; ++i) {
+        RTCPReportBlock rb;
+        std::memcpy(&rb, buffer + sizeof(RTCPHeader) + 4 + sizeof(RTCPReportBlock) * i, sizeof(RTCPReportBlock));
+//        rb.ssrc_ = ntohl(rb.ssrc_);
+//        rb.fraction_lost_ = rb.fraction_lost_;
+//        rb.cumulative_packets_lost_ = ntohl(rb.cumulative_packets_lost_) & 0x00FFFFFF;
+//        rb.extended_highest_sequence_number_ = ntohl(rb.extended_highest_sequence_number_);
+//        rb.interarrival_jitter_ = ntohl(rb.interarrival_jitter_);
+//        rb.last_sr_timestamp_ = ntohl(rb.last_sr_timestamp_);
+//        rb.delay_since_last_sr_ = ntohl(rb.delay_since_last_sr_);
+        rtcp_report_blocks_[rb.ssrc_] = rb;
+    }
+
+    return true;
 }
 
 void RtpRtcpHandler::receive_rtcp_packet() {
     // Receive and process RTCP packets
+    const size_t buffer_size = 4096;
+    char buffer[buffer_size];
+
+    struct sockaddr_in client_addr;
+    socklen_t client_address_len = sizeof(client_addr);
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(rtcp_port_);
+    if (inet_pton(AF_INET, ip_.c_str(), &client_addr.sin_addr) <= 0) {
+        throw std::runtime_error("RtpRtcpHandler bind client rtcp socket address failed");
+    }
+
+    while (is_running_) {
+        // Receive an RTCP packet
+        size_t received_size = recvfrom(rtcp_socket_, buffer, buffer_size, 0,
+                                        (struct sockaddr*)&client_addr, &client_address_len);
+
+        // Parse the RTCP packet
+        RTCPReceiverReport rr;
+        if (!parse_rtcp_packet(buffer, received_size, rr)) continue;
+    }
 }
 
 void RtpRtcpHandler::send_rtcp_packet() {
+    const size_t buffer_size = 4096;
+    char buffer[buffer_size];
+    struct sockaddr_in client_addr;
+    socklen_t client_address_len = sizeof(client_addr);
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(rtcp_port_);
+    if (inet_pton(AF_INET, ip_.c_str(), &client_addr.sin_addr) <= 0) {
+        throw std::runtime_error("RtpRtcpHandler bind client rtcp socket address failed");
+    }
+
+    RTCPSenderReport sr;
+    sr.version_ = 2;
+    sr.padding_ = 0;
+    sr.packet_type_ = 200;
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<uint32_t> dist(1, UINT32_MAX);
+    uint32_t ssrc = dist(mt);
+    sr.ssrc_ = htonl(ssrc);
+    const uint64_t NTP_epoch = 2208988800ull; // Offset between Unix and NTP epoch (January 1, 1900)
+
     // Send RTCP packets
+    while (is_running_) {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        struct timeval tv;
+        gettimeofday(&tv, nullptr);
+        uint64_t ntp_time = ((uint64_t)tv.tv_sec + NTP_epoch) << 32;
+        ntp_time += (uint64_t)tv.tv_usec * (1ull << 32) / 1000000ull;
+        uint32_t ntp_timestamp_msw = (uint32_t)(ntp_time >> 32);
+        uint32_t ntp_timestamp_lsw = (uint32_t)(ntp_time & 0xFFFFFFFF);
+        sr.ntp_timestamp_msw_ = htonl(ntp_timestamp_msw);
+        sr.ntp_timestamp_lsw_ = htonl(ntp_timestamp_lsw);
+        sr.rtp_timestamp_ = htonl(timestamp_);
+        sr.sender_packet_count_ = htonl(total_rtp_packet_sent_);
+        sr.sender_octet_count_ = htonl(total_payload_size_);
+        {
+            std::lock_guard<std::mutex> lock(rtcp_send_receive_mutex_);
+            sr.report_count_ = rtcp_report_blocks_.size();
+            sr.length_ = htons(6 + 6 * rtcp_report_blocks_.size());
+            memcpy(buffer, &sr, sizeof(RTCPSenderReport));
+            int i = 0;
+            // Just return back the original Report blocks, meaningless data.
+            for (auto [k, v] : rtcp_report_blocks_) {
+                memcpy(buffer + sizeof(RTCPSenderReport) + i * sizeof(RTCPReportBlock), &v, sizeof(RTCPReportBlock));
+                ++i;
+            }
+        }
+        size_t rtcp_packet_size = sizeof(RTCPSenderReport) + rtcp_report_blocks_.size() * sizeof(RTCPReportBlock);
+        ssize_t ret = sendto(rtcp_socket_, buffer, rtcp_packet_size, 0,
+                              (struct sockaddr *)&client_addr_, sizeof(client_addr_));
+    }
 }
--- RtpRtcpHandler.h
+++ RtpRtcpHandler.h
@@ -12,6 +12,8 @@
 #include <arpa/inet.h>
 #include <cstdint>
 #include <vector>
+#include <thread>
+#include <mutex>
 #include "MediaSourceHandler.h"
 
 const size_t MAX_RTP_PACKET_SIZE = 1024;
@@ -30,6 +32,36 @@ struct RTPHeader {
 //    uint32_t* csrc_list_;
 };
 
+struct RTCPHeader {
+    uint8_t report_count_ : 5;
+    uint8_t padding_ : 1;
+    uint8_t version_ : 2;
+    uint8_t packet_type_;
+    uint16_t length_;
+};
+
+struct RTCPReportBlock {
+    uint32_t ssrc_;
+    uint8_t fraction_lost_;
+    uint32_t cumulative_packets_lost_ : 24;
+    uint32_t extended_highest_sequence_number_;
+    uint32_t interarrival_jitter_;
+    uint32_t last_sr_timestamp_;
+    uint32_t delay_since_last_sr_;
+};
+
+struct RTCPReceiverReport : RTCPHeader {
+    uint32_t ssrc_;
+};
+
+struct RTCPSenderReport : RTCPHeader {
+    uint32_t ssrc_;
+    uint32_t ntp_timestamp_msw_; // Most Significant Word of NTP timestamp
+    uint32_t ntp_timestamp_lsw_; // Least Significant Word of NTP timestamp
+    uint32_t rtp_timestamp_;
+    uint32_t sender_packet_count_;
+    uint32_t sender_octet_count_;
+};
 
 class RtpRtcpHandler {
 public:
@@ -42,7 +74,6 @@ public:
     ~RtpRtcpHandler();
 
     void send_rtp_packet(const char* payload, size_t payload_size, MediaType payload_type);
-    void receive_rtcp_packet();
 
 private:
     int rtp_socket_;
@@ -56,12 +87,21 @@ private:
     uint32_t timestamp_;
     uint32_t timestamp_increment_;
 
-    void send_rtcp_packet();
     char rtp_buf_[MAX_RTP_PACKET_SIZE + 10];
 
     void send_h264(const char *payload, size_t payload_size);
-
     void send_aac(const char *payload, size_t payload_size);
+
+    bool is_running_;
+    std::shared_ptr<std::thread> rtcp_send_thread_;
+    std::shared_ptr<std::thread> rtcp_receive_thread_;
+    void send_rtcp_packet();
+    void receive_rtcp_packet();
+    std::mutex rtcp_send_receive_mutex_;
+    std::unordered_map<uint32_t, RTCPReportBlock> rtcp_report_blocks_;
+    uint32_t total_payload_size_;
+    uint32_t total_rtp_packet_sent_;
+    bool parse_rtcp_packet(const char *buffer, int received_bytes, RTCPReceiverReport &rr);
 };
 
 #endif // RTP_RTCP_HANDLER_H
